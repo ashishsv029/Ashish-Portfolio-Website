f23f37ea9b11928a25ce76611eb264440ca03edf
Nag Ashish S V <50989117+ashishsv029@users.noreply.github.com>
2020-08-30 13:40:33 +0530

    Add files via upload

---
+++ portfolio/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.slider-dots',
+        dotsClass:'dots',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+
+
+        
+    })
+})
